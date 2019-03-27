72c4b70613f0662e03a95b898ab35bc7033e8409
SatoMomoya <goodragon2158@gmail.com>
2019-03-27 16:21:01 +0900

    落ちたら初期値に戻るようにしました

--- CESA2019/Assets/Momoya/PlayerController.cs
+++ CESA2019/Assets/Momoya/PlayerController.cs
@@ -2,156 +2,187 @@
 using System.Collections;
 using UnityEngine.UI;
 using System;
-using PlayerState;
+/////////////
+using Momoya;
+using Momoya.PlayerState;
 
-
-public class PlayerController : MonoBehaviour
+namespace Momoya
 {
-    //列挙型の宣言
-    enum MoveDirection
+
+
+    public class PlayerController : MonoBehaviour
     {
-        UP,         //上
-        DOWN,       //下
-        LEFT,       //左
-        RIGHT,      //右
-
-        NUM,
-    }
-    //定数の定義
-    [SerializeField]
-    const float MoveSpeed = 1.0f; //動くスピード
-    //変数の宣言
-    private Vector3 _vec;         //速度
-    private Rigidbody _rg;        //リジットボディ
-    [SerializeField]
-    private KeyCode[] _moveKey = new KeyCode[(int)MoveDirection.NUM];//移動キー
-    //変更前のステート名
-    private string _beforeStateName;
-
-
-    public StateProcessor _stateProcessor = new StateProcessor();           //プロセッサー
-
-    //ステートの宣言
-    public StateDefault _stateDefault = new StateDefault();                 //デフォルト状態
-    public StateWalk _stateWalk = new StateWalk();                          //歩き状態
-
-    // Use this for initialization
-    void Start()
-    {
-     
-
-        _rg = GetComponent<Rigidbody>(); //リジットボディの取得
-
-        //初期ステートをdefaultにする
-        _stateProcessor.State = _stateDefault;
-        //委譲処理
-        _stateDefault.execDelegate = Default;
-        _stateWalk.execDelegate = Walk;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-        //速度を足す
-        _rg.velocity += _vec;
-
-        //ステートの値が変更されたら実行処理を行う
-        if (_stateProcessor.State == null)
+        //列挙型の宣言
+        enum MoveDirection
         {
-            return;
+            UP,         //上
+            DOWN,       //下
+            LEFT,       //左
+            RIGHT,      //右
+
+            NUM,
         }
+        //定数の定義
 
-        //現在どのステートか確認するためのデバッグ処理
-        if (_stateProcessor.State.GetStateName() != _beforeStateName)
-        {
-            Debug.Log(" Now State:" + _stateProcessor.State.GetStateName());
-            _beforeStateName = _stateProcessor.State.GetStateName();
-       
-        }
 
-        _stateProcessor.Execute();//実行関数
-    }
+        [SerializeField]
+        const float MoveSpeed = 0.3f; //動くスピード
+        const float Speedlimit = 3.0f;
+        const float DropdownPoint = -5.0f; //落下ポイント
+        //変数の宣言
+        private Vector3 _startPos;    //初期位置
+        private int _pos;
+        private Vector3 _vec;         //速度
+        private Rigidbody _rg;        //リジットボディ
+        [SerializeField]
+        private KeyCode[] _moveKey = new KeyCode[(int)MoveDirection.NUM];//移動キー
+                                                                         //変更前のステート名
+        private string _beforeStateName;
 
-    //移動関数
-    public void Move()
-    {
-        if(Input.GetKey(_moveKey[(int)MoveDirection.UP]))
-        {
-            _vec.z = MoveSpeed;
-        }
 
-        if (Input.GetKey(_moveKey[(int)MoveDirection.DOWN]))
-        {
-            _vec.z = -MoveSpeed;
-        }
+        public StateProcessor _stateProcessor = new StateProcessor();           //プロセッサー
 
-        if (Input.GetKey(_moveKey[(int)MoveDirection.RIGHT]))
-        {
-            _vec.x = MoveSpeed;
-        }
+        //ステートの宣言
+        public StateDefault _stateDefault = new StateDefault();                 //デフォルト状態
+        public StateWalk _stateWalk = new StateWalk();                          //歩き状態
 
-        if (Input.GetKey(_moveKey[(int)MoveDirection.LEFT]))
+        // Use this for initialization
+        void Start()
         {
-            _vec.x = -MoveSpeed;
-        }
-        /////////ここより下は停止用処理
 
-        if (Input.GetKeyUp(_moveKey[(int)MoveDirection.UP]))
-        {
-            _vec.z = 0.0f;
-        }
+            _rg = GetComponent<Rigidbody>(); //リジットボディの取得
+            _startPos = _rg.position;        //初期位置の設定
 
-        if (Input.GetKeyUp(_moveKey[(int)MoveDirection.DOWN]))
-        {
-            _vec.z = 0.0f;
-        }
-
-        if (Input.GetKeyUp(_moveKey[(int)MoveDirection.RIGHT]))
-        {
-            _vec.x = 0.0f;
-        }
-
-        if (Input.GetKeyUp(_moveKey[(int)MoveDirection.LEFT]))
-        {
-            _vec.x = 0.0f;
-        }
-    }
-    
-    //止まっているかチェックする関数
-    public bool CheckStop()
-    {
-        //止まっていたらtrueを返す
-        if(_vec == Vector3.zero)
-        {
-            return true;
-        }
-        //止まっていないことを伝える
-        return false;
-    }
-    /////////ステートの関数をここに記入
-
-    //通常状態
-    public void Default()
-    {
-        //移動キーのどれかが押されたら移動状態に切り替える
-        for(int i= 0; i <(int)MoveDirection.NUM; i++)
-        {
-            _stateProcessor.State = _stateWalk;
-        }
-    }
-
-    //歩き状態の関数
-    public void Walk()
-    {
-        //移動する
-        Move();
-        //止まっていたらステートを通常状態にする
-        if(CheckStop() == true)
-        {
+            //初期ステートをdefaultにする
             _stateProcessor.State = _stateDefault;
+            //委譲処理
+            _stateDefault.execDelegate = Default;
+            _stateWalk.execDelegate = Walk;
         }
+
+        // Update is called once per frame
+        void Update()
+        {
+            PlayerCtrl();
+            
+            //ステートの値が変更されたら実行処理を行う
+            if (_stateProcessor.State == null)
+            {
+                return;
+            }
+
+            //現在どのステートか確認するためのデバッグ処理
+            if (_stateProcessor.State.GetStateName() != _beforeStateName)
+            {
+                Debug.Log(" Now State:" + _stateProcessor.State.GetStateName());
+                _beforeStateName = _stateProcessor.State.GetStateName();
+
+            }
+
+            _stateProcessor.Execute();//実行関数
+        }
+        public int Pos
+        {
+            get { return _pos; }
+            set { _pos = value; }
+        }
+
+        //移動関数
+        public void Move()
+        {
+            if (Input.GetKey(_moveKey[(int)MoveDirection.UP]))
+            {
+                _vec.z = MoveSpeed;
+            }
+
+            if (Input.GetKey(_moveKey[(int)MoveDirection.DOWN]))
+            {
+                _vec.z = -MoveSpeed;
+            }
+
+            if (Input.GetKey(_moveKey[(int)MoveDirection.RIGHT]))
+            {
+                _vec.x = MoveSpeed;
+            }
+
+            if (Input.GetKey(_moveKey[(int)MoveDirection.LEFT]))
+            {
+                _vec.x = -MoveSpeed;
+            }
+            /////////ここより下は停止用処理
+
+            if (Input.GetKeyUp(_moveKey[(int)MoveDirection.UP]))
+            {
+                _vec.z = 0.0f;
+            }
+
+            if (Input.GetKeyUp(_moveKey[(int)MoveDirection.DOWN]))
+            {
+                _vec.z = 0.0f;
+            }
+
+            if (Input.GetKeyUp(_moveKey[(int)MoveDirection.RIGHT]))
+            {
+                _vec.x = 0.0f;
+            }
+
+            if (Input.GetKeyUp(_moveKey[(int)MoveDirection.LEFT]))
+            {
+                _vec.x = 0.0f;
+            }
+        }
+
+        public void PlayerCtrl()
+        {
+            //速度を足す
+            _rg.velocity += _vec;
+
+            //落下ポイントよりポジションが低ければ初期位置に戻す
+            if (_rg.position.y < DropdownPoint)
+            {
+                _rg.position = _startPos;
+            }
+        }
+
+        //止まっているかチェックする関数
+        public bool CheckStop()
+        {
+            //止まっていたらtrueを返す
+            if (_vec == Vector3.zero)
+            {
+                return true;
+            }
+            //止まっていないことを伝える
+            return false;
+        }
+        /////////ステートの関数をここに記入
+
+        //通常状態
+        public void Default()
+        {
+            //移動キーのどれかが押されたら移動状態に切り替える
+            for (int i = 0; i < (int)MoveDirection.NUM; i++)
+            {
+                if(Input.GetKey(_moveKey[i]))
+                {
+                    _stateProcessor.State = _stateWalk;
+                }
+                           
+            }
+        }
+
+        //歩き状態の関数
+        public void Walk()
+        {
+            //移動する
+            Move();
+            //止まっていたらステートを通常状態にする
+            if (CheckStop() == true)
+            {
+                _stateProcessor.State = _stateDefault;
+            }
+        }
+
+
     }
-
-
 }
--- CESA2019/Assets/Momoya/PlayerState.cs
+++ CESA2019/Assets/Momoya/PlayerState.cs
@@ -2,68 +2,73 @@
 using System.Collections;
 
 
-//プレイヤーのステート
-namespace PlayerState
+
+namespace Momoya
 {
 
-    //ステートの実行を管理するクラス
-    public class StateProcessor
+    //プレイヤーのステート
+    namespace PlayerState
     {
-        //ステート本体
-        private PlayerState _state;
-        // ステートを取得、セットをするプロパティ
-        public PlayerState State
+
+        //ステートの実行を管理するクラス
+        public class StateProcessor
         {
-            set { _state = value; }
-            get { return _state; }
-        }
-
-        // 実行関数
-        public void Execute()
-        {
-            State.Execute();
-        }
-
-    }
-
-    //ステートのクラス
-    public abstract class PlayerState
-    {
-        //委譲
-        public delegate void ExecuteState();
-        public ExecuteState execDelegate;
-
-        //実行関数
-        public virtual void Execute()
-        {
-            if (execDelegate != null)
+            //ステート本体
+            private PlayerState _state;
+            // ステートを取得、セットをするプロパティ
+            public PlayerState State
             {
-                execDelegate();
+                set { _state = value; }
+                get { return _state; }
+            }
+
+            // 実行関数
+            public void Execute()
+            {
+                State.Execute();
+            }
+
+        }
+
+        //ステートのクラス
+        public abstract class PlayerState
+        {
+            //委譲
+            public delegate void ExecuteState();
+            public ExecuteState execDelegate;
+
+            //実行関数
+            public virtual void Execute()
+            {
+                if (execDelegate != null)
+                {
+                    execDelegate();
+                }
+            }
+
+            //現在のステートをストリング型で返す(c++で純粋仮想関数みたいなやつ)
+            public abstract string GetStateName();
+        }
+
+        // 以下状態クラス
+
+        //  デフォルト状態
+        public class StateDefault : PlayerState
+        {
+            public override string GetStateName()
+            {
+                return "Player is Default";
             }
         }
 
-        //現在のステートをストリング型で返す(c++で純粋仮想関数みたいなやつ)
-        public abstract string GetStateName();
-    }
-
-    // 以下状態クラス
-
-    //  デフォルト状態
-    public class StateDefault : PlayerState
-    {
-        public override string GetStateName()
+        //  歩き状態
+        public class StateWalk : PlayerState
         {
-            return "Player is Default";
+            public override string GetStateName()
+            {
+                return "Plaer Is Walk";
+            }
         }
-    }
 
-    //  歩き状態
-    public class StateWalk : PlayerState
-    {
-        public override string GetStateName()
-        {
-            return "Plaer Is Walk";
-        }
     }
-
 }
