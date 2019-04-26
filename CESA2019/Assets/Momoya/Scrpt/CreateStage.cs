3e00d2b0e3405269df3cecaec63a72dfbe510078
SatoMomoya <goodragon2158@gmail.com>
2019-04-26 19:59:44 +0900

    マップ生成変更

--- CESA2019/Assets/Momoya/Scrpt/CreateStage.cs
+++ CESA2019/Assets/Momoya/Scrpt/CreateStage.cs
@@ -23,7 +23,20 @@ public class CreateStage : MonoBehaviour
             Num
         }
 
-        public GameObject floor;
+        public enum GroundType
+        {
+            GroundNone = -1, //何もなし
+            GroundWhite001,  //白床
+            GroundBlue001,  //青床001
+            GroundRed001,   //赤床001
+            GroundGreen001, //緑床001
+            GroundBlack001, //黒床001
+
+            Num
+        }
+
+
+        
 
         //定数の定義
         [SerializeField]
@@ -31,15 +44,22 @@ public class CreateStage : MonoBehaviour
         //変数の定義
         private Vector3 startPos;
         private int _searchWidth;
-
+    
         [SerializeField]
-        private string _fileName = "StageData001"; //ファイルの名前
+        private string _objFileName = "ObjectData"; //ファイルの名前(オブジェクトよう)
+        [SerializeField]
+        private string _stageFileName = "StageData"; //ファイルの名前(スーテジ用)
+        [SerializeField]
+        private int _stageNumber = 1; //ステージ番号
         private string _openFilenameExtension;
-        private string _filePath; //ファイルパス        
+        private string _objFilePath; //オブジェクト用ファイルパス
+        private string _stageFilePath; //オブジェクト用ファイルパス         
         [SerializeField]
         private GameObject[] _gameObj = new GameObject[(int)ObjectType.Num]; //オブジェクトを入れる
-
-        private List<int> _objectDataLest; //オブジェクトデータリスト
+        [SerializeField]
+        private GameObject[] _floorObj = new GameObject[(int)GroundType.Num];//地面用の生成オブジェクト
+        private List<int> _objectDataList; //オブジェクトデータリスト
+        private List<int> _stageDataList;  //ステージデータリスト
     // Start is called before the first frame update
     void Start()
     {
@@ -47,9 +67,11 @@ public class CreateStage : MonoBehaviour
             _searchWidth = 0;
             _openFilenameExtension = ".csv";
                 
-            _filePath = Application.dataPath + @"\Data\StageData\" + _fileName + _openFilenameExtension;
+            _objFilePath = Application.dataPath + @"\Data\StageData\" + _objFileName + _stageNumber.ToString() + _openFilenameExtension;
+            _stageFilePath = Application.dataPath + @"\Data\StageData\" + _stageFileName + _stageNumber.ToString() + _openFilenameExtension;
 
-            _objectDataLest = new List<int>(); //データリスト作成  
+            _objectDataList = new List<int>(); //データリスト作成  
+            _stageDataList = new List<int>();  //データリスト作成
             ReadFile();//ファイルを読み込む
             SearchWidth();
             BuildFloor(); //床を作る
@@ -69,31 +91,40 @@ public class CreateStage : MonoBehaviour
 
             int objData;
             //　一括で取得
-            string[] texts = File.ReadAllText(_filePath).Split(new char[] { ',', '\n' }, StringSplitOptions.RemoveEmptyEntries);
-            foreach (var text in texts)
+            string[] objTexts = File.ReadAllText(_objFilePath).Split(new char[] { ',', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var text in objTexts)
             {
                 int tmp = 0;
                 Int32.TryParse(text,out tmp);
 
-                _objectDataLest.Add(tmp);
+                _objectDataList.Add(tmp);
             }
-            objData = _objectDataLest.Count;
+            objData = _objectDataList.Count;
 
             //デバッグ
             for(int i = 0; i < objData; i++)
             {
-                Debug.Log(_objectDataLest[i]);
+                Debug.Log(_objectDataList[i]);
+            }
+
+            string[] stageTexts = File.ReadAllText(_stageFilePath).Split(new char[] { ',', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var text in stageTexts)
+            {
+                int tmp = 0;
+                Int32.TryParse(text, out tmp);
+
+                _stageDataList.Add(tmp);
             }
 
         }
         //作成関数
         public void BuildStage()
         {
-            for(int i = _objectDataLest.Count -1; i >= 0;i--)
+            for(int i = _objectDataList.Count -1; i >= 0;i--)
             {
-                if(_objectDataLest[i] != -1)
+                if(_objectDataList[i] != -1)
                 {
-                    GameObject go = Instantiate(_gameObj[_objectDataLest[i]]);
+                    GameObject go = Instantiate(_gameObj[_objectDataList[i]]);
                     go.transform.position = this.transform.position;
                 }
                 if((i) % _searchWidth != 0)
@@ -111,17 +142,20 @@ public class CreateStage : MonoBehaviour
         //床を作る関数
         public void BuildFloor()
         {
-            for (int i = _objectDataLest.Count - 1; i >= 0; i--)
+            for (int i = _objectDataList.Count - 1; i >= 0; i--)
             {
-                GameObject go = Instantiate(floor);
-                go.transform.position = this.transform.position;
+                if (_objectDataList[i] != -1)
+                {
+                    GameObject go = Instantiate(_floorObj[_stageDataList[i]]);
+                    go.transform.position = this.transform.position;
+                }
                 if ((i) % _searchWidth != 0)
                 {
                     transform.position = new Vector3(this.transform.position.x + 1.0f, this.transform.position.y, this.transform.position.z);
                 }
                 else
                 {
-                    transform.position = new Vector3(startPos.x, this.transform.position.y, this.transform.position.z - 1.0f);
+                    transform.position = new Vector3(startPos.x, this.transform.position.y, this.transform.position.z - 1);
                 }
 
             }
@@ -135,7 +169,7 @@ public class CreateStage : MonoBehaviour
             //widthがオブジェクトデータの総数になるまで回し続ける
             while(true)
             {
-                if(_searchWidth * _searchWidth == _objectDataLest.Count)
+                if(_searchWidth * _searchWidth == _objectDataList.Count)
                 {
                     //同じだったら抜ける
                     break;
